62f69083055be855781d26f33fd766bfd7492e8b
unknown <contactashishjoshi99@gmail.com>
2020-03-29 21:35:19 +0530

    added main admin pannel

---
+++ admin/index.php
@@ -0,0 +1,178 @@
+<?php
+    include_once '../includes/connection.php';
+    session_start();
+
+    if(isset($_SESSION['author_id'])){
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+    
+    <!--For any type of error message-->
+    <?php
+        if(isset($_GET['message'])){
+            $msg= $_GET['message'];
+            echo'<div class="alert alert-danger" role="alert">
+                 '.$msg.'   
+                </div>';
+        }
+    ?>
+
+    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
+    
+    <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+        <a class="nav-link" href="logout.php">Sign out</a>
+        </li>
+    </ul>
+    </nav>
+
+    <div class="container-fluid">
+    <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+        <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">
+                <span data-feather="home"></span>
+                Dashboard <span class="sr-only">(current)</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <span data-feather="file"></span>
+                Orders
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <span data-feather="shopping-cart"></span>
+                Products
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <span data-feather="users"></span>
+                Customers
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <span data-feather="bar-chart-2"></span>
+                Reports
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <span data-feather="layers"></span>
+                Integrations
+                </a>
+            </li>
+            </ul>
+        </div>
+        </nav>
+
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Dashboard</h1>
+            
+        </div>
+        <div id='info-form'>
+            <form method='post'>
+                <div class="form-group">
+                    
+                    Name: <input type="name" name='author_name' class="form-control" id="email " value='<?php echo $_SESSION['author_name'];?>' placeholder='Enter Password'>
+                </div>
+                <div class="form-group">
+                    
+                    Email: <input type="email" name='author_email' class="form-control" id="exampleInputEmail1" placeholder='Enter Email' value='<?php echo $_SESSION['author_email'];?>' aria-describedby="emailHelp">
+        
+                </div>
+                <div class="form-group">
+                    
+                    Password: <input type="password" name='author_password' class="form-control" id="exampleInputPassword1 "  placeholder='Password'>
+                </div>
+                
+                Your Bio: <textarea class="form-control" id="exampleFormControlTextarea1" name='author_bio' rows="3"  placeholder='Enter your bio here'><?php echo $_SESSION['author_bio'];?></textarea><br>
+                <button type="submit" name="update" class="btn btn-primary">Update</button>
+
+            </form>
+            <?php
+                // if update button is clicked update the information
+                if(isset($_POST['update'])){
+                    $author_name= mysqli_real_escape_string($conn,$_POST['author_name']);
+                    $author_email= mysqli_real_escape_string($conn,$_POST['author_email']);
+                    $author_password= mysqli_real_escape_string($conn,$_POST['author_password']);
+                    $author_bio= mysqli_real_escape_string($conn,$_POST['author_bio']);
+
+                    if(empty($author_name) OR empty($author_email) OR empty($author_bio)){
+                        echo "Empty Fields!"; 
+                    }
+                    else{
+                        // checking if new email entered is valid or not
+                        if(!filter_var($author_email,FILTER_VALIDATE_EMAIL)){
+                            echo "Please enter valid email";
+                        }
+                        else{
+                            // if user wants to change the password
+                            if(empty($author_password)){
+                                $author_id=$_SESSION['author_id'];
+                                $sql = "UPDATE `author` SET `author_name`='$author_name', `author_email`='$author_email', `author_bio`='$author_bio' WHERE `author_id`='$author_id'";
+
+                                if(mysqli_query($conn,$sql)){
+                                    $_SESSION['author_name']=$author_name;
+                                    $_SESSION['author_email']=$author_email;
+                                    $_SESSION['author_bio']=$author_bio;
+
+                                    header("Location: index.php?message=Changes+Updated");
+                                }
+                                else{
+                                    echo "Error";
+                                }
+                            }
+                            else{
+                                // if user doesnt want to change the password hash his new password
+
+                                $hash= password_hash($author_password,PASSWORD_DEFAULT);
+                                $author_id = $_SESSION['author_id'];
+                                $sql = "UPDATE `author` SET author_name='$author_name', author_email='$author_email', author_bio='$author_bio', author_password='$hash' WHERE author_id='$author_id';";
+
+                                if(mysqli_query($conn,$sql)){
+                                    session_unset();
+                                    session_destroy();
+                                    header("Location: login.php?message=Changes+Updated+Login+Again+Please!");
+                                }
+                                else{
+                                    echo "Some error occured";
+                                }
+
+                            }
+                        }
+                    }
+                }
+            ?>
+        </div>
+        
+        </main>
+    </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
+<?php
+
+}else{
+    header("Location: login.php?message=Please+Login+first!");
+}
+?>
