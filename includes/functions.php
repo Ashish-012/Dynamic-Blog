8efefa5d2bbbd3c49b965461c3d3c006bfad4a65
unknown <contactashishjoshi99@gmail.com>
2020-03-31 21:19:58 +0530

    added separate file for functions

---
+++ includes/functions.php
@@ -0,0 +1,13 @@
+<?php
+    include_once "connection.php";
+
+    function jumbotron(){
+        echo '<div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                <h1 class="display-4">Fluid jumbotron</h1>
+                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                </div>
+              </div>';
+    }
+
+?>
