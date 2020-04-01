2b31ac9c34c7b8515657713765bccc021994c7bd
unknown <contactashishjoshi99@gmail.com>
2020-04-01 21:17:53 +0530

    changed name of the tab and added reference

--- admin/nav.php
+++ admin/nav.php
@@ -2,15 +2,15 @@
     <div class="sidebar-sticky">
             <ul class="nav flex-column">
             <li class="nav-item">
-                <a class="nav-link active" href="#">
+                <a class="nav-link active" href="index.php">
                 <span data-feather="home"></span>
                 Dashboard <span class="sr-only">(current)</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="posts.php">
                 <span data-feather="file"></span>
-                Orders
+                Posts
                 </a>
             </li>
             <li class="nav-item">
