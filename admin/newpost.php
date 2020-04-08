1089585be8664be09162826b40973a38dac68930
unknown <contactashishjoshi99@gmail.com>
2020-04-08 20:05:23 +0530

    removed extra plugins

--- admin/newpost.php
+++ admin/newpost.php
@@ -155,11 +155,8 @@
     <script>
         tinymce.init({
         selector: 'textarea',
-        plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
-        toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
-        toolbar_mode: 'floating',
-        tinycomments_mode: 'embedded',
-        tinycomments_author: 'Author name',
+        plugins: 'casechange lists checklist pageembed table',
+        toolbar: 'casechange checklist pageembed table',
         });
     </script>
 </body>
