039791e45931a25d11726cdfda20c4289e745bbc
unknown <contactashishjoshi99@gmail.com>
2020-04-18 21:45:28 +0530

    fixed posts not creating bug

--- admin/newpost.php
+++ admin/newpost.php
@@ -25,7 +25,7 @@
     ?>
 
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="../index.php">MyDynamicSite</a>
     
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
@@ -44,103 +44,102 @@
             
         </div>
         <div id='info-form'>
-            <form method='post' enctype='multipart/form-data'>
-                <div class="form-group">
-                    
-                    Post Title : <input type="text" name='post_title' class="form-control"  placeholder='Enter Post Title'>
-                </div>
-                Post Category:
-                <div class="form-group">
-                    <select class="form-control" name='post_category' id="exampleFormControlSelect2">
-                    <?php 
-                        $sql = "SELECT * FROM `category`";
-                        $result= mysqli_query($conn, $sql);
-                        while($row=mysqli_fetch_assoc($result)){
-                            $category_id=$row['category_id'];
-                            $category_name=$row['category_name'];
+            <form method="post" enctype="multipart/form-data">
+					Post Title
+					 <input type="text" name="post_title" class="form-control" placeholder="Post Title"><br>
+					 
+					Post Category
+					<select name="post_category" class="form-control" id="exampleFormControlSelect1">
+					<?php
+						$sql = "SELECT * FROM `category`";
+						$result = mysqli_query($conn, $sql);
+						while($row=mysqli_fetch_assoc($result)){
+							$category_id = $row['category_id'];
+							$category_name = $row['category_name'];
+							?>
+							<option value="<?php echo $category_id; ?>"><?php echo $category_name; ?></option>
+							<?php
+						}
+					?>
+					</select><br>
+					
+					Post Content
+					<textarea name="post_content" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea><br>
+					
+					Post Image
+					<input type="file" name="file" class="form-control-file" id="exampleFormControlFile1"><br>
+					
+					Post Keywords
+					 <input type="text" name="post_keywords" class="form-control" placeholder="Enter Keywords"><br>
+					 
+					 
+					 <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+				</form>
+				<?php
+					if(isset($_POST['submit'])){
+						$post_title = mysqli_real_escape_string($conn, $_POST['post_title']);
+						$post_category = mysqli_real_escape_string($conn, $_POST['post_category']);
+						$post_content = mysqli_real_escape_string($conn, $_POST['post_content']);
+						$post_keywords = mysqli_real_escape_string($conn, $_POST['post_keywords']);
+						$post_author = $_SESSION['author_id'];
+						$post_date = date("d/m/y");
+						
+						//checking if above fields are empty
+						if(empty($post_title) OR empty($post_category) OR empty($post_content)){
+							
+							echo "<script>window.location='newpost.php?message=Empty+Fields'</script>";
+							exit();
+						}
                         
-                    ?>
-                    <option value='$category_id'><?php echo "$category_name"?></option>
-                    <?php
-                        }
-                    ?>
-                    </select>
-                </div>
-                Post content: <textarea class="form-control" id="exampleFormControlTextarea1" name='post_content' rows="3"  placeholder='Enter content here'></textarea><br>
-                Upload image:
-                <div class="form-group">
-                    <input type="file" name='file' class="form-control-file" id="exampleFormControlFile1">
-                </div>
-                <div class="form-group">
-                    
-                    Post Keywords : <input type="text" name='post_keywords' class="form-control"  placeholder='Enter keyword'>
-                </div>
-                <button type="submit" name="update" class="btn btn-primary">Create Post</button>
-            </form>
-            <?php
-                if(isset($_POST['update'])){
-                    $post_title = mysqli_real_escape_string($conn, $_POST['post_title']);
-                    $post_category = mysqli_real_escape_string($conn, $_POST['post_category']);
-                    $post_content = mysqli_real_escape_string($conn, $_POST['post_content']);
-                    $post_keywords = mysqli_real_escape_string($conn, $_POST['post_keywords']);
-                    $post_author = $_SESSION['author_id'];
-                    $post_date= date("d/m/y");
+                        // file handling
+						$file = $_FILES['file'];
+				
+						$fileName = $file['name'];
+						$fileType = $file['type'];
+						$fileTmp = $file['tmp_name'];
+						$fileErr = $file['error'];
+						$fileSize = $file['size'];
+						
+						$fileEXT = explode('.',$fileName);
+                        $fileExtension = strtolower(end($fileEXT));
+                        
+						//supported extensions
+						$allowedExt = array("jpg", "jpeg", "png", "gif");
+						
+						if(in_array($fileExtension, $allowedExt)){
+							if($fileErr === 0){
+								if($fileSize < 3000000){
+                                    // if the file meet all the requirements upload it
+									$newFileName = uniqid('',true).'.'.$fileExtension;
+									$destination = "../uploads/$newFileName";
+									$dbdestination = "uploads/$newFileName";
+									move_uploaded_file($fileTmp, $destination);
+									$sql = "INSERT INTO post (`post_title`,`post_content`,`post_category`, `post_author`, `post_date`, `post_keywords`, `post_image`) VALUES ('$post_title', '$post_content', '$post_category', '$post_author', '$post_date', '$post_keywords', '$dbdestination');";
+									if(mysqli_query($conn, $sql)){
+									
+										echo "<script>window.location='posts.php?message=Post+Published!'</script>";
+									}else{
+										
+										echo "<script>window.location='newpost.php?message=Error'</script>";
+									}
+								} else {
+									
+									echo "<script>window.location='newpost.php?message=YOUR FILE IS TOO BIG TO UPLOAD!!'</script>";
+									exit();
+								}
+							}else{
+								
+								echo "<script>window.location='newpost.php?message=Oops Error Uploading your file. Supported formats are jpeg, png, jpg, gif'</script>";
+								exit();
+							}
+						}else{
+							echo "<script>window.location='newpost.php?message=Oops Error Uploading your file. File is too big to upload'</script>";
+							exit();
+						}
+					}
+							
+				?>
 
-                    //checking if fields are empty
-
-                    if(empty($post_title) OR empty($post_category) OR empty($post_content)){
-                        header("Location: newpost.php?message=Empty+Fields!");
-                        exit();
-                    }
-                    $file = $_FILES['file'];
-                    // storing various details in different variables
-                    $file_Name= $file['name'];
-                    $file_Type= $file['type'];
-                    $file_Tmp= $file['tmp_name'];
-                    $file_Error= $file['error'];
-                    $file_Size= $file['size'];
-
-                    // to check the extension of file
-
-                    $fileExt = explode('.',$file_Name);
-                    $file_extension = strtolower(end($fileExt));
-
-                    // accepted file formats
-
-                    $allowedExt = array("jpg","jpeg","png","gif");
-
-                    if(in_array($file_extension,$allowedExt)){
-                        if($file_Error === 0 ){
-                            if($file_Size<5000000){
-                                $new_file_name = uniqid('',true).'.'.$file_extension;
-                                $destination = "../uploads/$new_file_name";
-                                $db_destination = "uploads/$new_file_name";
-                                move_uploaded_file($file_Tmp,$destination);
-
-                                $sql = "INSERT INTO `post` (`post_author`,`post_image`,`post_title`,`post_content`,`post_category`,`post_keywords`,`post_date`) VALUES ('$post_author','$db_destination','$post_title','$post_content','$post_category','$post_keywords','$post_date');";
-                                if(mysqli_query($conn, $sql)){
-                                    header("Location: posts.php?message=Post+Added");
-                                    exit();
-                                }
-                                else{
-                                    header("Location: newposts.php?message=Error!");
-                                    exit();
-                                }
-                            }
-                            else{
-                                echo "File too big to upload";
-                            }
-                        }
-                        else{
-                            echo "Error uploading your file!";
-                        }
-                    }
-                    else{
-                        echo "You have uploaded wrong file!";
-                    }
-
-                }
-            ?>
             
         </div>
         </main>
@@ -151,7 +150,7 @@
     <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="https://cdn.tiny.cloud/1/b7ifonmi3q0pcuylkj33essu2ip05cax7b30b0i77th9k3jc/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="https://cdn.tiny.cloud/1/TOKENGOESHERE/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <script>
         tinymce.init({
         selector: 'textarea',
